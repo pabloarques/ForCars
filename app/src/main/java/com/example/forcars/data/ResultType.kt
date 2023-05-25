b8914cee98597c92f36fa18abfb0ccff75d81a9e
Pablo <pabloarques98@gmail.com>
2023-05-25 23:32:06 +0200

    FCRS-2 crear llamda repositorio e implementar dagger hilt

--- app/src/main/java/com/example/forcars/data/ResultType.kt
+++ app/src/main/java/com/example/forcars/data/ResultType.kt
@@ -1,7 +1,6 @@
 package com.example.forcars.data
 
 sealed class ResultType<T> {
-
     data class Success<T>(val data: T) : ResultType<T>()
     data class Error<T>(val message: String?) : ResultType<T>()
 }
--- app/src/main/java/com/example/forcars/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/forcars/ui/home/HomeFragment.kt
@@ -6,9 +6,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.forcars.databinding.FragmentHomeBinding
-import com.example.forcars.ui.notifications.NotificationsViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -18,20 +17,34 @@ class HomeFragment : Fragment() {
     private val binding get() = _binding!!
 
     private val viewModel by viewModels<HomeViewModel>()
+    private lateinit var adapter: CarsAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val homeViewModel =
-            ViewModelProvider(this)[HomeViewModel::class.java]
-
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
-
-
-
+        setupRecyclerView()
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        observeViewModel()
+        viewModel.getCars()
+    }
+
+    private fun setupRecyclerView() {
+        adapter = CarsAdapter()
+        binding.rvHome.adapter = adapter
+        binding.rvHome.layoutManager = LinearLayoutManager(requireContext())
+    }
+
+    private fun observeViewModel() {
+        viewModel.cars.observe(viewLifecycleOwner) { cars ->
+            adapter.submitList(cars)
+        }
     }
 
     override fun onDestroyView() {
