a172eaccd258a7673954ba61530aca7dfb052123
pablo <pabloarques98@gmail.com>
2023-06-02 18:40:05 +0200

    FCRS-11 Implementar firebase

---
+++ app/src/main/java/com/example/forcars/di/module/model/AppModule.kt
@@ -0,0 +1,20 @@
+package com.example.forcars.di.module.model
+
+import android.app.Application
+import android.content.Context
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideContext(Application: Application): Context {
+        return Application.applicationContext
+    }
+}
--- app/src/main/java/com/example/forcars/ui/common/utils/Constants.kt
+++ app/src/main/java/com/example/forcars/ui/common/utils/Constants.kt
@@ -4,4 +4,6 @@ object Constants {
 
     const val BASE_URL = "https://forcars.pockethost.io/api/files/osl5jb9wrgtbd5j/"
     const val REQUEST_CALL_PHONE_PERMISSION = 1
+    const val PREFS_NAME = "my_prefs"
+    const val KEY_IS_LOGGED_IN = "is_logged_in"
 }
